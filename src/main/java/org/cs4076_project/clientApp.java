f253ffcea904b66c0dc85f5a58c221c21b11b8e4
BenCurran0 <bmcmorrancurran@gmail.com>
2025-04-27 02:16:05 +0100

    Update clientApp.java
    days off a little

--- src/main/java/org/cs4076_project/clientApp.java
+++ src/main/java/org/cs4076_project/clientApp.java
@@ -20,7 +20,7 @@ import javafx.stage.*;
 public class clientApp extends Application {
     private Stage stage;
     private GridPane scheduleGrid;
-    private String[] weekdays = {" Monday", " Tuesday", "Wednesday", " Thursday", "  Friday"};
+    private String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
     private String[] moduleTime = {"9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"};
 
     final int minWidth = 800;
